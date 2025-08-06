bac0b5687fee1aa10bfed8c90d3c872f555ced96
alaesm <161771548+alaesm@users.noreply.github.com>
2025-08-06 02:37:34 +0200

    alae

--- eslint-config.ts
+++ eslint-config.ts
@@ -7,11 +7,11 @@ import tseslint from "typescript-eslint";
 export default tseslint.config(
   { ignores: ["dist"] },
   {
-    "extends": [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
-      "globals": globals.browser
+      globals: globals.browser
     },
     plugins: {
       "react-hooks": reactHooks,
@@ -26,4 +26,5 @@ export default tseslint.config(
       "@typescript-eslint/no-unused-vars": "off"
     }
   }
-)
+);
+
